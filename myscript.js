ef04c052b0b59861cfa382685840b8b4c077a30a
Riccardo <riccardo@baako.de>
2021-02-18 23:10:28 +0100

    func created

---
+++ myscript.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
